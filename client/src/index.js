86564f1d6df1189b5b5ff012173b2bbcbb448f4f
f3tm3a <toooh.jood@gmail.com>
2024-12-04 15:20:25 +0400

    update users component

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./Store/Store";
 import { Provider } from "react-redux";
-import Register from "./Components/Register";
-//<Register />
+//import Register from "./Components/Register";
+//import Login from "./Components/Login";
+
+import { persistore } from "./Store/Store";
+import { PersistGate } from "redux-persist/integration/react";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      {/* Wrapping with PersistGate */}
+      <PersistGate loading={null} persistor={persistore}>
+        <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>
 );
